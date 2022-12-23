9cad6bb1b1da56d5ade644259b9f6e0978a6d905
xinru911017 <s1102292@gm.ppu.edu.tw>
2022-12-23 15:46:46 +0800

    version 5:title

--- index.py
+++ index.py
@@ -41,8 +41,22 @@ def webhook():
   req = request.get_json(force=True)
   action =  req.get("queryResult").get("action")
   if (action == "cartoonChoice"):
-      date =  req.get("queryResult").get("parameters").get("date")
-      info = "您選擇的星期數是：" + date
+    rate =  req.get("queryResult").get("parameters").get("date")
+    if (rate == "一"):
+        rate = "每週一"
+    elif (rate == "二"):
+        rate = "每週二"
+    info = "您選擇的星期天數是：" + rate + "，相關動漫：\n"
+
+    collection_ref = db.collection("動漫卡片")
+    docs = collection_ref.get()
+    result = ""
+    for doc in docs:
+        dict = doc.to_dict()
+        if rate in dict["date"]:
+            result += "片名：" + dict["title"] + "\n"
+            result += "介紹：" + dict["link"] + "\n\n"
+    info += result
   return make_response(jsonify({"fulfillmentText": info}))
 
 @app.route("/")
